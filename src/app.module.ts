d27e1cf5f3a673e57568e164d0ec4366b340deb9
Neider Ruiz <neiderbtc@gmail.com>
2023-08-27 09:29:52 -0500

    config passport, jwtModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { jwtConstants } from './auth/constans';
+import { JwtStrategy } from './auth/jwt.strategy';
+import { BadRequestFilter } from './interceptors/badrequestfilter.interceptor';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { User } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
@@ -26,13 +40,27 @@ import { UserModule } from './user/user.module';
           migrationsDir: 'src/migrations',
         },
         charset: 'utf8mb4',
-        entities: [],
+        entities: [
+          User
+        ],
       }),
       inject: [ConfigService],
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    JwtStrategy,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: BadRequestFilter,
+    },
+
+  ],
 })
 export class AppModule { }
