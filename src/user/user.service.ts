7bd87b9375477e52494bb6ff73402fb92c75f60d
Neider Ruiz <neiderbtc@gmail.com>
2023-08-27 09:17:44 -0500

    hash password user create

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) { }
+
+  async create(user: CreateUserDto): Promise<User> {
+    const salt = await bcrypt.genSalt(10);
+    return this.userRepository.save({
+      email: user.email.toLowerCase(),
+      name: user.name,
+      password: await bcrypt.hash(user.password, salt),
+    });
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.userRepository.findOneBy({ id });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
@@ -22,5 +36,29 @@ export class UserService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  async checkPassword(user: User, password: string): Promise<boolean> {
+    return bcrypt.compareSync(password, user.password);
+  }
+
+  async validateUser(email: string, password: string): Promise<User | null> {
+    const user = await this.userRepository.findOneBy({ email });
+
+    if (!user) {
+      return null;
+    }
+
+    const isPasswordValid = await this.checkPassword(user, password);
+
+    if (!isPasswordValid) {
+      return null;
+    }
+
+    return user;
+  }
+
+  async findOneByEmail(email: string): Promise<User | null> {
+    return await this.userRepository.findOneBy({ email });
   }
 }
