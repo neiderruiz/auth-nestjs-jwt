64634747388d2cc861215eebefd55490b628df2d
Neider Ruiz <neiderbtc@gmail.com>
2023-08-27 09:18:07 -0500

    dto create user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsOptional, Length } from 'class-validator';
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsEmail()
+    readonly email: string;
+
+    @IsOptional()
+    readonly name: string;
+
+    @IsNotEmpty()
+    @Length(8, 20)
+    readonly password: string;
+
+    @IsNotEmpty()
+    @Length(8, 20)
+    readonly passwordConfirmation: string;
+
+}
